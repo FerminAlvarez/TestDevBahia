00937b3ed088428c99a239696ef6f0b88d50f2f9
FerminAlvarez <ferminalvarez.dev@gmail.com>
2024-06-19 12:41:02 -0300

    Fix: Change database name from promos name to promotions

--- promo-tiles/app/Config/Database.php
+++ promo-tiles/app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => 'root',
-        'database'     => 'promosdb',
+        'database'     => 'promotionsdb',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
