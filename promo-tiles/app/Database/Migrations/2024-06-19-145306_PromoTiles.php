76baa5ef1e83b574dbf2842e74ec42bd36d01b84
FerminAlvarez <ferminalvarez.dev@gmail.com>
2024-06-19 12:23:43 -0300

    Add Promo migration

---
+++ promo-tiles/app/Database/Migrations/2024-06-19-145306_PromoTiles.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Promo extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 10,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'title' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'description' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'image_url' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ]
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('promos');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('promos');
+    }
+}
